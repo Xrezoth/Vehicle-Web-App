62e8752a2d5e69582b9ecb9f560616dd9377df78
Xrezoth <148421626+Xrezoth@users.noreply.github.com>
2024-06-25 11:18:51 -0600

    commit -m "Primer prueba de la app"

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Vehicle-Web-App
 Proyecto del Sprint 5
+
+Descripción de la aplicaión web:
+
+Utilizando el conjunto de datos proporcionado en el archivo vehicles_us.csv, se construyen dos gráficos. Ambos presentan como cambia el precio de un vehículo según las millas recorridas. 
+
+El primer gráfico es un histograma y el segundo es un gráfico de dispersión. 
---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') #Leer los datos
+
+st.header('Autos en Estados Unidos')# Mostrar encabezado
+
+hist_button = st.button('Construir histograma') #Construir un botón para histograma
+scatter_button = st.button('Gráfico de dispersión') #Construir un botón para un gráfico de dispersión
+
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button: #Al hacer clic en el botón
+        # escribir un mensaje
+        st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+        # Crear un gráfico de dispersión
+        fig = px.scatter(car_data, x="odometer")
+
+        # Mostrar un gráfico interactivo
+        st.plotly_chart(fig, use_container_width=True)
+
+
